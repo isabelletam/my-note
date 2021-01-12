450586adb2339dcab4da5a8354f72f3ef0fc7692
isabelletam <77304986+isabelletam@users.noreply.github.com>
2021-01-11 18:48:30 -0800

    Delete project_v5f3b directory

--- project_v5f3b/src/main/model/EntryType.java
+++
@@ -1,6 +0,0 @@
-package model;
-
-// Enum for all my EntryTypes
-public enum EntryType {
-    NOUN, VERB, ADJECTIVE, ADVERB, PARTICLE, NONE, MISC;
-}
--- project_v5f3b/src/main/persistence/Writable.java
+++
@@ -1,9 +0,0 @@
-package persistence;
-
-import org.json.JSONObject;
-
-// THE CODE WAS BASED OFF OF Json serialization demo!
-public interface Writable {
-    // EFFECTS: returns this as JSON object
-    JSONObject toJson();
-}
